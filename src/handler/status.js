594be4107681e90abb8c377ff43e250b9b7f3d18
Christopher <108158222+supspaceyt@users.noreply.github.com>
2023-03-03 17:25:05 -0600

    Update status.js

--- src/handler/status.js
+++ src/handler/status.js
@@ -1,56 +1,56 @@
-// COMMENTS WERE WRITTEN BY OPENAI CHAT BOT THE OTHER CODE IS COPYRIGHT TO OUR TEAM
+// PART OF COMMENTS WERE WRITTEN BY OPENAI CHAT BOT THE OTHER CODE IS COPYRIGHT TO OUR TEAM
 // IF YOU HAVE ANY QUESTIONS CONTACT OUR TEAM contact.ct@supspace.ga
 
 module.exports = [{
     text: "For staff apps", // Set bot activity text to "For staff apps"
     type: "WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "The vids", // Set bot activity text to "The vids"
     type: "WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 Seconds
     },{
     text: "Our Staff", // Set bot activity text to "Our Staff"
     type:"WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Hello", // Set bot activity text to "Hello"
     type: "WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Yo", // Set bot activity text to "Yo"
     type: "PLAYING", // Set bot activity type to "PLAYING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Call of duty", // Set bot activity text to "Call of duty"
     type: "PLAYING", // Set bot activity type to "PLAYING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Rule Breakers", // Set bot activity text to "Rule Breakers"
     type: "WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "With my dog", // Set bot activity text to "With my dog"
     type: "PLAYING", // Set bot activity type to "PLAYING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Joe", // Set bot activity text to "Joe"
     type: "WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Spotify", // Set bot activity text to "Spotify"
     type: "LISTENING", // Set bot activity type to "LISTENING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Smooth jazz", // Set bot activity text to "Smooth jazz"
     type: "LISTENING", // Set bot activity type to "LISTENING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "Join the discord! discord.gg/vJzqtew6nq", // Set bot activity text to "Join the discord! discord.gg/vJzqtew6nq"
     type: "PLAYING", // Set bot activity type to "PLAYING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     },{
     text: "The world burn and come back", // Set bot activity text to "The world burn and come back"
     type: "WATCHING", // Set bot activity type to "WATCHING"
-    time: 12 // Set bot activity duration to 12 hours
+    time: 12 // Set bot activity duration to 12 seconds
     }]
