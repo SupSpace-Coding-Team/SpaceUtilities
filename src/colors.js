e5e7b3137809616c3f86e32226842ccbe817c9f7
Christopher <108158222+supspaceyt@users.noreply.github.com>
2023-03-03 17:11:33 -0600

    Create colors.js

---
+++ src/colors.js
@@ -0,0 +1,29 @@
+// COMMENTS WERE WRITTEN BY OPENAI CHAT BOT THE OTHER CODE IS COPYRIGHT TO OUR TEAM
+// IF YOU HAVE ANY QUESTIONS CONTACT OUR TEAM contact.ct@supspace.ga
+
+// Export a function that takes a 'loader' parameter
+module.exports = (loader) => {
+    // Set custom colors for the loader
+    loader.setColors({
+        walking: ["blink", "dim", "fgWhite"],
+        failedWalking: {
+            name: ["bright", "fgYellow", "underline"],
+            text: ["gray", "fgRed"]
+        },
+        typeError: {
+            command: ["bright", "fgYellow"],
+            type: ["fgYellow"],
+            text: ["bright", "fgRed"]
+        },
+        failLoad: {
+            command: ["bright", "fgMagenta"],
+            type: ["fgRed"],
+            text: ["bright", "fgRed"],
+        },
+        loaded: {
+            command: ["bright", "fgRed"],
+            type: ["bright", "fgWhite"],
+            text: ["bright", "fgBlue"]
+        },
+    })
+}
