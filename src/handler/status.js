1c33a3ce08fefaaf17ca317f4b010e6fd0f36110
Christopher <108158222+supspaceyt@users.noreply.github.com>
2023-03-03 17:21:25 -0600

    Create status.js

---
+++ src/handler/status.js
@@ -0,0 +1,53 @@
+module.exports = [{
+    text: "For staff apps", // Set bot activity text to "For staff apps"
+    type: "WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "The vids", // Set bot activity text to "The vids"
+    type: "WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Our Staff", // Set bot activity text to "Our Staff"
+    type:"WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Hello", // Set bot activity text to "Hello"
+    type: "WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Yo", // Set bot activity text to "Yo"
+    type: "PLAYING", // Set bot activity type to "PLAYING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Call of duty", // Set bot activity text to "Call of duty"
+    type: "PLAYING", // Set bot activity type to "PLAYING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Rule Breakers", // Set bot activity text to "Rule Breakers"
+    type: "WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "With my dog", // Set bot activity text to "With my dog"
+    type: "PLAYING", // Set bot activity type to "PLAYING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Joe", // Set bot activity text to "Joe"
+    type: "WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Spotify", // Set bot activity text to "Spotify"
+    type: "LISTENING", // Set bot activity type to "LISTENING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Smooth jazz", // Set bot activity text to "Smooth jazz"
+    type: "LISTENING", // Set bot activity type to "LISTENING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "Join the discord! discord.gg/vJzqtew6nq", // Set bot activity text to "Join the discord! discord.gg/vJzqtew6nq"
+    type: "PLAYING", // Set bot activity type to "PLAYING"
+    time: 12 // Set bot activity duration to 12 hours
+    },{
+    text: "The world burn and come back", // Set bot activity text to "The world burn and come back"
+    type: "WATCHING", // Set bot activity type to "WATCHING"
+    time: 12 // Set bot activity duration to 12 hours
+    }]
